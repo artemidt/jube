db170be57fb4b9a330eb4cbadf98be8b3d3e2bf5
Artem Sokolov <artem.sokolov@idt.net>
2023-08-29 00:09:22 +0500

    Initial commit

---
+++ Jube.App/HealthCheck/DatabaseHealthCheck.cs
@@ -0,0 +1,30 @@
+﻿using Jube.Engine.HealthCheck;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Jube.Data.HealthCheck
+{
+    internal class DatabaseHealthCheck : IHealthCheck
+    {
+        private readonly DatabaseAvailabilityChecker _databaseAvailabilityChecker;
+
+        public DatabaseHealthCheck(DatabaseAvailabilityChecker databaseAvailabilityChecker)
+        {
+            _databaseAvailabilityChecker = databaseAvailabilityChecker;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(
+            HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            var isHealthy = await _databaseAvailabilityChecker.IsPostgresAvailableAsync(cancellationToken);
+
+            if (isHealthy)
+            {
+                return HealthCheckResult.Healthy("A healthy result.");
+            }
+
+            return new HealthCheckResult(context.Registration.FailureStatus, "An unhealthy result.");
+        }
+    }
+}
---
+++ Jube.Engine/HealthCheck/DatabaseAvailabilityChecker.cs
@@ -0,0 +1,40 @@
+﻿using Jube.Data.Context;
+using Jube.DynamicEnvironment;
+using Jube.Engine.Helpers;
+using Nest;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Jube.Engine.HealthCheck
+{
+    public class DatabaseAvailabilityChecker
+    {
+        private readonly DynamicEnvironment.DynamicEnvironment _dynamicEnvironment;
+
+        public DatabaseAvailabilityChecker(DynamicEnvironment.DynamicEnvironment dynamicEnvironment)
+        {
+            _dynamicEnvironment = dynamicEnvironment;
+        }
+
+        public Task<bool> IsPostgresAvailableAsync(CancellationToken cancellationToken = default)
+        {
+            
+            try
+            {
+                using (var dbContext = DataConnectionDbContext.GetDbContextDataConnection(_dynamicEnvironment.AppSettings("ConnectionString")))
+                {
+                    using var command = dbContext.Connection.CreateCommand();
+                    command.CommandText = "SELECT 1;";
+                    var result = command.ExecuteScalar();
+                }
+            }
+            catch (Exception ex)
+            {
+                return Task.FromResult(false);
+            }
+
+            return Task.FromResult(true);
+        }
+    }
+}
